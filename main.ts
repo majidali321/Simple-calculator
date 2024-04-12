f9c5afa12c11e128714947f61a1d8297b67ed219
majid ali <mianmajid577@gmail.com>
2024-04-12 10:50:58 +0500

    calculator

---
+++ main.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+console.log("\n\tWelcome to my Simple \'Calculator\'\n")
+
+let answer = await inquirer.prompt([
+    {name:"FirstNnumber",type:"number",message:"enterb ur first num"},
+    {name:"secondNumber",type:"number",message:"enter your second num"},
+    {
+        name:"operator",
+        type:"list",
+        message:"enter your operator",
+        choices:["addition","subtration","multiplication","division"]
+    }
+])
+if (answer.operator === "addition"){
+    console.log(answer.FirstNnumber + answer.secondNumber )
+}else if(answer.operator === "subtration"){
+    console.log(answer.FirstNnumber - answer.secondNumber)
+}else if(answer.operator === "multiplication"){
+    console.log(answer.FirstNnumber * answer.secondNumber)
+}else if (answer.operator === "division"){
+    console.log(answer.FirstNnumber / answer.secondNumber)
+}else{
+    console.log("invalid input")
+}
